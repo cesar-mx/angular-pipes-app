c48278418ac0bdd19740d127d8c30fbd6b09668c
Cesar Ernesto Hernandez Frias <cesar.net@live.com>
2021-09-04 13:59:26 -0700

    aprendi pipes keyValue, json and async

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -15,5 +16,50 @@ export class NoComunesComponent {
     'masculino':'invitarlo',
     'femenino':'invitarla'
   }
+
+  clientes:string[] =["Cesar","Juan","Pedro", "Paco"];
+
+  clientesMap = {
+    '=0':'no tenemos clientes',
+    '=1':'tenemos un cliente',
+    'other': "# clientes esperando"
+  }
+  cambiarPersona()
+  {
+    if(this.nombre == "Cesar")
+    {
+        this.nombre = "Melissa";
+        this.genero = "femenino";
+    }
+    else
+    {
+      this.nombre = "Cesar";
+      this.genero = "masculino";
+    }    
+  }
+
+  popLast()
+  {
+    this.clientes.pop();
+  }
+
+  persona= {
+    nombre :  "César",
+    edad :  27,
+    direccion :  "Tijuana, BC, México",
+
+  }
+
+  //async pipe
+
+  myObservable = interval(1000);
+
+  valorPromesa = new Promise( (resolve, reject)=>{
+
+    setTimeout(()=>{
+      resolve("fin de la promesa!")
+    }, 3000);
+
+  });
 
 }
