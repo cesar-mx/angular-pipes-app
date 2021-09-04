03aed2122eebc5f40f3cd1d4d2136e60dc3240b6
César Hernández <cesar.net@live.com>
2021-09-04 11:46:20 -0700

    On branch master  Changes to be committed: 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	modified:   src/styles.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+//cambiar idiomas de la app
+import idiomaEs from '@angular/common/locales/es-MX'
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(idiomaEs);  
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+
+    BrowserModule,
+    AppRouterModule, 
+    PrimeNgModule,
+    SharedModule,
+    VentasModule,
+    BrowserAnimationsModule
+
   ],
-  providers: [],
+  providers: [  
+    {
+      provide: LOCALE_ID, useValue:'es-MX'  /*para idioma español*/ 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
